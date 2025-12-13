42dd127762727acc938d155af94cccc3e4c25ea1
Pizenn <muhammadhafiznur796@gmail.com>
2025-12-13 23:26:03 +0700

    update link api

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://thoric-exoterically-brittaney.ngrok-free.dev/api';
+export const API_BASE_URL = 'https://thoric-exoterically-brittaney.ngrok-free.dev';
 
 export const SERVER_URL = 'https://thoric-exoterically-brittaney.ngrok-free.dev'
