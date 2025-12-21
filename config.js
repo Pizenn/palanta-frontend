449c27be4e1e0c37fc89123ebdbe9fdfc2019e04
Pizenn <muhammadhafiznur796@gmail.com>
2025-12-21 15:35:05 +0700

    update link backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'palanta-homestay-a8csacbygxamd8fd.indonesiacentral-01.azurewebsites.net/api';
+export const API_BASE_URL = 'https://palanta-homestay-a8csacbygxamd8fd.indonesiacentral-01.azurewebsites.net//api';
 
-export const SERVER_URL = 'palanta-homestay-a8csacbygxamd8fd.indonesiacentral-01.azurewebsites.net';
+export const SERVER_URL = 'https://palanta-homestay-a8csacbygxamd8fd.indonesiacentral-01.azurewebsites.net/';
